90a2bcc2c2c06d4bda0a2edb692ee4c7e08f46d9
ominiks <tobigee@gmail.com>
2025-03-28 15:00:56 +0300

    Updated tracker code to include rectangle mod-point detection

--- tracker.py
+++ tracker.py
@@ -1,3 +1,4 @@
+import math
 class EuclideanDistTracker:    
     def __init__(self):        
         self.center_points = {}        
@@ -10,4 +11,13 @@ class EuclideanDistTracker:
             x, y, w, h = rect        
             cx = (x + x + w) // 2        
             cy = (y + y + h) // 2        
-            print(cx, cy)
+            #print(cx, cy)
+            same_object_detected = False            
+            for id, pt in self.center_points.items():                
+                dist = math.hypot(cx - pt[0], cy - pt[1])                
+                if dist < 50:
+                     self.center_points[id] = (cx, cy)                    
+                     objects_bbs_ids.append([x, y, w, h, id])                    
+                     same_object_detected = True                    
+                     break        
+        return objects_bbs_ids
